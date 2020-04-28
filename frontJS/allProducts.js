4242765c7c6400ec676e2289182712ad58f81bf8
cosmonet-fr <stephane.billois@hotmail.com>
2020-04-28 18:07:39 +0200

    Affichage du contenu du tableau des objets. (Bugué)

--- frontJS/allProducts.js
+++ frontJS/allProducts.js
@@ -1,14 +1,22 @@
 console.log("test");
+
+let newProduct = document.getElementById('listeProducts')
 let products = new XMLHttpRequest();
 
 products.onreadystatechange = function () {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+      console.log("Ça marche ! Serveur : " + this.status + " | Ready State : " + this.readyState);
       let responseProducts = JSON.parse(this.responseText);
-      console.log(products);
+
+      for (var i = 0; i < responseProducts.length; i++) {
+        responseProducts[i]
+        console.log(responseProducts[i].name);
+        newProduct.innerHTML = '<div class="product"><div class="picture"> <img src="' + responseProducts[i].imageUrl + '" alt=""><p class="price" >' + responseProducts[i].price + ' €</p></div><div class="description"><h2>' + responseProducts[i].name + '</h2><p>' + responseProducts[i].description + '</p></div></div><hr />'
+      }
 
 
     } else {
-      console.error("Ça na marche pas");
+      console.error("Ça na marche pas, serveur : " + this.status + " | Ready State : " + this.readyState);
     }
 };
 
