7bab3842065e8353fc4fae064600e945252b7f95
cosmonet-fr <stephane.billois@hotmail.com>
2020-04-29 12:32:59 +0200

    La liste de l'index fonctionne, mais il n'y a pas encore de Promises. Couleur de l'anim de chargement = au logo.

--- frontJS/allProducts.js
+++ frontJS/allProducts.js
@@ -1,6 +1,7 @@
-console.log("test");
+console.log("run allProducts.js");
+let newProduct = document.getElementById("listeProduct");
+let loadingImg = document.getElementById("loading");
 
-let newProduct = document.getElementById('listeProducts')
 let products = new XMLHttpRequest();
 
 products.onreadystatechange = function () {
@@ -8,10 +9,19 @@ products.onreadystatechange = function () {
       console.log("Ça marche ! Serveur : " + this.status + " | Ready State : " + this.readyState);
       let responseProducts = JSON.parse(this.responseText);
 
+
+      loadingImg.classList.replace("open", "close");// Fermer l'annimation de chargement par le remplacement de la class open par close
+      console.log(loadingImg);                      // Vérifier
+
+
       for (var i = 0; i < responseProducts.length; i++) {
         responseProducts[i]
         console.log(responseProducts[i].name);
-        newProduct.innerHTML = '<div class="product"><div class="picture"> <img src="' + responseProducts[i].imageUrl + '" alt=""><p class="price" >' + responseProducts[i].price + ' €</p></div><div class="description"><h2>' + responseProducts[i].name + '</h2><p>' + responseProducts[i].description + '</p></div></div><hr />'
+        //newProduct.appendChild = '<div class="product"><div class="picture"> <img src="' + responseProducts[i].imageUrl + '" alt=""><p class="price" >' + responseProducts[i].price + ' €</p></div><div class="description"><h2>' + responseProducts[i].name + '</h2><p>' + responseProducts[i].description + '</p></div></div><hr />'
+        let newDivOfProduct = document.createElement("div");                 // Création de la div Produit
+        newDivOfProduct.classList.add("product");                            // Ajout de la class="product" à la div
+        newDivOfProduct.innerHTML = ' <div class="picture"> <img src="' + responseProducts[i].imageUrl + '" alt=""><p class="price" >' + responseProducts[i].price + ' €</p></div><div class="description"><h2>' + responseProducts[i].name + '</h2><p>' + responseProducts[i].description + '</p></div>' // Ajout du code HTML pour le produit
+        document.querySelector(".allProducts").appendChild(newDivOfProduct); // Ajout de la div "product" comme enfant de la div "allProducts"
       }
 
 
@@ -23,3 +33,7 @@ products.onreadystatechange = function () {
 
 products.open("GET", "http://localhost:3000/api/cameras" );
 products.send();
+
+
+
+//'<div class="product"><div class="picture"> <img src="' + responseProducts[i].imageUrl + '" alt=""><p class="price" >' + responseProducts[i].price + ' €</p></div><div class="description"><h2>' + responseProducts[i].name + '</h2><p>' + responseProducts[i].description + '</p></div></div><hr />'
