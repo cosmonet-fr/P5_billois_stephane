c404f3ef60c858c2fe152ae4c4d0c076f2e11916
cosmonet-fr <stephane.billois@hotmail.com>
2020-05-14 18:38:39 +0200

    nouveux cart: Ok pour ajout d'article dans localStorage(visble dans consoleWeb).

--- frontJS/addToCart.js
+++ frontJS/addToCart.js
@@ -5,11 +5,45 @@ let _id = urlParam('_id');
 let name = urlParam('name');
 let price = urlParam('price');
 
+
+const curentProducts = JSON.parse(localStorage.getItem('cart.products'));
+console.log(curentProducts);
+
+let cart = {createdAt: new Date(), products : []};
+
 if (id != 'null' && _id != 'null' && name != 'null' && price != 'null') {
   console.log('Yo !!!');
+  //Ajout des produit actuels dans l'objet JS
+  let oldCart = JSON.parse(localStorage.getItem('cart'));
+  for (let i = 0; i <= localStorage.length; i++) {
+    cart.products.push({
+      id: oldCart.products[i].id,
+      _id: oldCart.products[i]._id,
+      name: oldCart.products[i].name,
+      price: oldCart.products[i].price
+  });
+  }
 
-  localStorage.setItem(id, _id);
-  console.log('Le ' + name + ' à été ajouté dans votre panier.');
+  cart.products.push({
+    id: id,
+    _id: _id,
+    name: name,
+    price: price
+});
+
+
+localStorage.setItem('cart', JSON.stringify(cart));
+
+//console.log('Dans l\'autre sens !!!!!');
+
+
+//console.log(localStorage.getItem('cart'));
+
+//const fin = JSON.parse(localStorage.getItem('cart'));
+
+//console.log('fin = ' + fin.products[1].name);
+
+
 
 } else {
   console.error('NO !!!');
--- frontJS/readCart.js
+++ frontJS/readCart.js
@@ -1,40 +1,5 @@
 console.log('run readCart.js');
 
-fetch('http://localhost:3000/api/cameras')
-  .then(response => response.json())
-    .then(apiData =>{
-      if (localStorage.length > 0) {
-
-        console.log('Dans mon panier, il ya donc : ');
-        let total = 0;
-        for (let indexCart = 0; indexCart < localStorage.length; indexCart++) {
-
-          if (localStorage.key(indexCart) != 'darkMod' ) {
-
-            console.log(localStorage.key(indexCart));
-            let eltCart = document.createElement("div");                 // Création de la div eltCart
-            eltCart.classList.add("eltCart");                            // Ajout de la class="eltCart" à la div
-            eltCart.setAttribute('id', apiData[localStorage.key(indexCart)]._id);                            // Ajout de la class="pour identifier le produit" à la div
-            let id = "'" + apiData[localStorage.key(indexCart)]._id + "'";
-            var removeKey  = localStorage.key(indexCart);
-            
-            eltCart.innerHTML = '<div id"nameCart" class="nameCart"><h2 class="nameOnCart" >' + apiData[localStorage.key(indexCart)].name +'</h2></div><div class="infosCart"><p id="priceOnCart" class="price" >' + apiData[localStorage.key(indexCart)].price + ' € </p> <i class="far fa-trash-alt fa-2x" onclick="removeProductOfCart(' + id + ');"></i></div>'
-            document.querySelector(".myCart").appendChild(eltCart);
-
-          } else {
-            console.log("Ce localStorage est réservé au mod sombre !");
-          }
-
-        }
-        // Affichage du total
-        //var eltCart = document.createElement("div");
-        //eltCart.classList.add("eltCart");
-        //eltCart.innerHTML = '<div class="nameCart"><h2 id="nameOnCart" >Total :</h2></div><p id="priceOnCart" class="price" >' + 'total' + '</p>';
-        //document.querySelector(".page").appendChild(eltCart);
-
-      }
 
 
-
-
-    });
+console.log(localStorage.getItem(cart));
