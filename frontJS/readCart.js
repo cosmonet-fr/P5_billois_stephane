522908a68594679f16f938bfbee49163cbd1bd58
cosmonet-fr <stephane.billois@hotmail.com>
2020-05-15 18:42:21 +0200

    small cart ok.

--- frontJS/readCart.js
+++ frontJS/readCart.js
@@ -5,9 +5,19 @@ let myCart = document.getElementById('myCart');
 console.log(myCart);
 
 for (let i = 0; i < myProducts.products.length; i++) {
-  let eltCart = document.createElement("div");                            // Création de la div eltCart
+  var eltCart = document.createElement("div");                            // Création de la div eltCart
   eltCart.classList.add("eltCart");                                       // Ajout de la class="eltCart" à la div
   eltCart.innerHTML = '<h2 class="nameOnCart" >' + myProducts.products[i].name + '</h2>  <p class="price" >' + myProducts.products[i].price + ' € <i class="far fa-trash-alt"></i></p>';
   document.querySelector(".myCart").appendChild(eltCart);
 
 }
+// Calcul du total du panier
+let sumCart = 0;
+for (let i = 0; i < myProducts.products.length; i++) {
+  let unitPrice = parseInt(myProducts.products[i].price, 10);
+  sumCart = sumCart + unitPrice;
+}
+console.log(sumCart);
+console.log(eltCart);
+eltCart.classList.add("eltCart");
+eltCart.innerHTML = '<h2>Total :</h2><p class="price">' + sumCart + ' €</p>';
