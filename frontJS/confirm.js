2d9e311c595531f308c0e4716ddf4e6edf6bfdad
cosmonet-fr <stephane.billois@hotmail.com>
2020-06-03 17:06:06 +0200

    GET

--- frontJS/confirm.js
+++ frontJS/confirm.js
@@ -1,14 +1,44 @@
+console.log(checkLettersOnly(urlParam('firstName')));
 
-//const form = document.getElementById('form');
-form.addEventListener('submit', function(event){
+if (checkLettersOnly(urlParam('firstName')) === true || checkLettersOnly(urlParam('lastName')) === true || checkLettersOnly(urlParam('city'))   ) {
+  console.log('ok');
 
-  console.log(event);
-  let formData = new FormData(document.getElementById('form'));
-  console.log(formData.entries());
+} else {
+  console.error('Données non-conformes');
+}
 
-});
 
-formData = new FormData(document.getElementById('form'));
-console.log(formData.entries());
+console.log(urlParam('firstName'));
 
-for(data of formData.entries()){console.log(data)}
+const firstName = urlParam('firstName');
+document.title = 'Merci ' + firstName + ' | Orinoco'; // changement de la valeur en <title> nomModel </title>
+const contact = {
+  firstName: firstName,
+  lastName: urlParam('lastName'),
+  address: urlParam('address'),
+  city: urlParam('code') + ' ' + urlParam('city'),
+  email: urlParam('email')
+};
+let cartLocalStorage = JSON.parse(localStorage.getItem('cart'))
+let products = [];
+for (let i = 0; i < cartLocalStorage.products.length; i++) {
+  products.push(cartLocalStorage.products[i]._id);
+}
+// Envoie des infos au serveur de l'api
+sendToApi(contact);
+sendToApi(products);
+// Afichage de la page_confirm
+// Remerciment
+let page_confirm = document.getElementById('page_confirm');
+let confirm = document.createElement('div');
+confirm.classList.add('confirm');
+document.querySelector('#page_confirm').appendChild(confirm);
+let text = document.createElement('p');
+text.innerText = 'Merci ' + firstName + ', d\'avoir choisi Orinoco pour votre commande ! À bientôt ! :-)'
+document.querySelector('.confirm').appendChild(text);
+fetch('http://localhost:3000/api/cameras')
+  .then(response => response.json())
+    .then(orderData => {
+      console.log(orderData);
+    });
+localStorage.removeItem('cart');
